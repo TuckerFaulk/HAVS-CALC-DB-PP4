35065d4e0934d5f4ac341daeae66a6b3a0076e44
TuckerFaulk <faulkner1993@hotmail.co.uk>
2022-12-15 14:38:59 +0000

    Add index and calculator html, css, js and link views and urls

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Equipment, Calculator
+# from .forms import CalculatorForm, EquipmentForm
 
-# Create your views here.
+
+def Index(request):
+    return render(request, 'index.html', {})
+
+
+class CalculatorListView(generic.ListView):
+    model = Calculator
+    queryset = Calculator.objects.order_by('make_and_model')
+    template_name = 'calculator.html'
+
+
+# .filter(author=user)
